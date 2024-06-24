812be87165f55f72767d38a511944c1b107d6ad5
Den-Prahms-ihr-Ivo <ivo@ivolino.de>
2024-06-24 10:24:42 +0200

    playlist generation script

---
+++ generate_playlist.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PLAYLIST_DIR="/var/lib/mpd/playlists"
+MEDIA_DIR="/home/ivo/media"
+
+for dir in "$MEDIA_DIR"/*     # list directories in the form "/tmp/dirname/"
+do
+    dir=${dir%*/}      # remove the trailing "/"
+    dir_name="${dir##*/}"    # print everything after the final "/"
+
+    # Is it a directory?
+    if [ -d "$dir" ]; then
+      find $dir -type f -name \*.mp3 > "$PLAYLIST_DIR/$dir_name.m3u"
+      echo "$dir Playlist created ..."
+    fi
+done
