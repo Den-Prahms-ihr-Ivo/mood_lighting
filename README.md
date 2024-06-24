1b9f530297c0c28d3b719b92f5416bf1c7ecba6b
Den-Prahms-ihr-Ivo <ivo@ivolino.de>
2024-06-24 09:33:01 +0200

    harmonization raspi & desktop

--- README.md
+++ README.md
@@ -1,1 +1,102 @@
 # mood_lighting
+
+## Was habe ich alles gemacht?
+
+### MPD
+
+To add the InnoMaker RPI HIFI DAC hat option to the Raspberry Pi you will need to add a line of text to a configuration file.
+
+Use the following command to open the config.txt file
+
+`sudo nano /boot/firmware/config.txt`
+
+Scroll down to the bottom of that file and add the following text. Make sure it’s pasted exactly as you see it in the screenshot:
+
+Beneath `[all]`
+`dtoverlay=allo-boss-dac-pcm512x-audio`
+
+With the DAC hat attached and enabled we’ll just need to find the device number. Check the device number with the following command:
+
+aplay -l
+
+Hit ENTER
+
+This should show you an initial list of devices. For the InnoMaker RPI HIFI DAC you’ll want to look for the BossDAC listing.
+
+NOTE: sometimes this command returns an error instead of a list of devices. I’m not sure why, but if you see an Invalid argument error that suggests removing or fixing ALSA, go ahead and remove it with this command:
+
+rm ~/.asoundrc
+
+If you are asked to confirm the deletion, type yes
+
+Hit ENTER
+
+Then run the following command in Terminal:
+
+cat /proc/asound/cards
+
+Hit ENTER
+
+This will pretty much just confirm the device number of the DAC. You can see in the screenshot that the device number in my case is 3
+
+Find the output device number for your DAC
+
+Set the device as the default soundcard
+Now we’ll sent the device number we found as the default soundcard, which should automatically select the DAC as our output device each time you launch the Raspberry Pi.
+
+Use the following command to create and open the sound config file:
+
+sudo nano /etc/asound.conf
+
+Hit Enter
+
+Now, paste the following code block at the bottom of the file:
+
+pcm.!default { type hw card 3 } ctl.!default { type hw card 3 }
+
+Remember to replace the card number with the device number you found in a previous step.
+
+With that new line added, hit the following key command to Write Out and save the file:
+
+Control + O
+
+Hit ENTER
+
+Then exit the config file with the following key command:
+
+Control + X
+
+This is a good time to reboot the Raspberry Pi with the following command:
+
+sudo reboot
+
+Hit Enter
+
+Access the Raspberry Pi config file to change the default audio settings
+Now you’ll set the newly added DAC as your default audio output in the Raspberry Pi configuration menu.
+
+Use the following command to open the Raspberry Pi configuration UI:
+
+sudo raspi-config
+
+Hit ENTER
+
+Select System Options → Audio
+
+Select BossDAC from the output list, then select Ok
+
+Important: The device number in the audio output settings should match the device you set as the default soundcard. If it doesn’t, reboot the Raspberry Pi again and re-check sudo raspi-config
+
+### MPD
+
+https://linuxaudiofoundation.org/musiclounge-configure-mpd/
+
+## CURRENT TODOs
+
+- Pin High and Low Kram testen
+- MPD Testen
+  - Hier besonders das Time Out und die eigentlich Funktion.
+  - Notieren der Steps, die ich zum zum Laufen bekommen eingegangen bin notieren
+  - In wie weit klappt das mit dem Display?
+- Schaltung für Peripherie testen und entwickeln.
+- Code in der Gänze auf dem Raspberry testen.
--- src/mood_lighting/light_module.py
+++ src/mood_lighting/light_module.py
@@ -2,6 +2,28 @@ import RPi.GPIO as GPIO
 
 from typing import Optional
 from gpiozero import LED, Button
+from threading import Thread
+from queue import Queue
+
+# pip install rpi-ws281x
+# Muss leider als sudo ausgeführt werden
+from rpi_ws281x import PixelStrip, ws, Color
+
+# TODO: LED COntroller sollte in nem eigenen Thread laufen!!!!!
+# Und dementsprechend auch gecancelt werden
+
+# TODO: color und animation müssen noch ausgeklügelt werden
+
+"""
+    for i in range(0, LED_COUNT_HALF):
+      led_strip.setPixelColor(i, Color(right_r, right_g, right_b))
+    
+    for i in range(LED_COUNT_HALF, led_strip.numPixels() - 3):
+      led_strip.setPixelColor(i, Color(left_r, left_g, left_b))
+
+      
+    led_strip.show()
+"""
 
 
 class Mood_Light(object):
@@ -9,6 +31,9 @@ class Mood_Light(object):
 
     _instance = None
     NEXT_BUTTON: Optional[Button] = None
+    _LED_STRIP = None
+    _LED_THREAD = None
+    _LED_QUEUE = None
 
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
@@ -18,6 +43,14 @@ class Mood_Light(object):
     def __init__(self) -> None:
         # GPIOs mit GPIO-Nummer ansprechen
         GPIO.setmode(GPIO.BCM)
+
+        # Geteilte Warteschlange
+        self._LED_QUEUE = Queue()
+
+        # Consumer definieren
+        self._LED_THREAD = Thread(
+            target=self.led_consumer, args=(self._LED_QUEUE, self._LED_STRIP)
+        )
 
         # Ein- und Ausgaenge definieren
         # GPIOs 18-21 Sind Tabu, die werden vom Audio Schild benutzt.
@@ -30,12 +63,69 @@ class Mood_Light(object):
         # shutdown_btn = Button(SHUTDOWN_BUTTON, hold_time=2)
         # empty_btn    = Button(EMPTY_BUTTON)
 
+        # NeoPixel Definition
+        led_strip = ws.WS2811_STRIP_GRB
+
+        self._LED_STRIP = PixelStrip(
+            LED_COUNT,
+            LED_COM,
+            LED_FREQ_HZ,
+            LED_DMA,
+            LED_INVERT,
+            LED_BRIGHTNESS,
+            LED_CHANNEL,
+            led_strip,
+        )
+        # Intialize the library (must be called once before other functions).
+        self._LED_STRIP.begin()
+        self._LED_THREAD.start()
+
     def turn_on(self) -> None:
         """"""
         # TODO: implement
         print(f"Turning Mood Lighting ON")
+        self._LED_QUEUE.put()
 
     def turn_off(self) -> None:
         """"""
         # TODO: implement
         print(f"Turning Mood Lighting OFF")
+        # SET LOW!!!!
+        # TODO: das zum initialisieren machen
+
+    # ###########################################
+    # Consumer Definition
+    # ###########################################
+    #
+    # TODO: Consumer bearbeiten und entsprechend die Funktionsaufrufe
+    # anpassen.
+    # ==> keine direkten aufrufe, sondern PUTs into queue
+    def led_consumer(self, queue: Queue, led_strip):
+        print("LED Consumer läuft...")
+        while True:
+            item = queue.get()
+
+            if item is None:
+                # LEDs auschhalten & Loop beenden
+                break
+
+            # item hat jetzt die Form (LED_Mode, LED_Strip Objekt, signal )
+            mode, led_strip, signal = item  # TODO: Mode shutdpwn
+
+            if mode == LED_Mode.playlist:
+                process_playlist_change(led_strip, signal)
+
+            elif mode == LED_Mode.song:
+                process_song_change(led_strip, signal)
+
+            elif mode == LED_Mode.startup:
+                animate_startup(led_strip)
+
+            elif mode == LED_Mode.ready:
+                animate_ready(led_strip, signal)
+
+            elif mode == LED_Mode.shutdown:
+                animate_shutdown(led_strip)
+                break
+
+        print("LED Consumer ist fertig!")
