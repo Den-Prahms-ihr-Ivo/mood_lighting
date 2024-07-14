b6173ed8a707a6fd346a2d2da6d5d49a5e733585
Den-Prahms-ihr-Ivo <ivo@ivolino.de>
2024-07-14 17:56:33 +0100

    added start up scripts

--- start_mqtt_server.sh
+++ start_mqtt_server.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
 cd /opt/zigbee2mqtt
 
-npm start
+export NVM_INC=/home/ivo/.nvm/versions/node/v22.2.0/include/node
+export NVM_DIR=/home/ivo/.nvm
+export NVM_BIN=/home/ivo/.nvm/versions/node/v22.2.0/bin
+
+
+/home/ivo/.nvm/versions/node/v22.2.0/bin/node /opt/zigbee2mqtt/index.js
+#/home/ivo/.nvm/versions/node/v22.2.0/bin/node
+#npm start
