0a0b5ab1225e20673d3e0d2271c305557b9a1f51
Den-Prahms-ihr-Ivo <ivo@ivolino.de>
2024-07-14 18:19:48 +0200

    resolved issue

--- src/mood_lighting/utility_components/music_component.py
+++ src/mood_lighting/utility_components/music_component.py
@@ -6,8 +6,10 @@ from src.helper.state_types import BASIS_STATES, BINARY_STATES
 from src.config import CONFIG
 import json
 from random import randrange
+from threading import Timer
 
 from pathlib import Path
+from mpd import MPDClient
 
 
 class MusicComponent(Utility_Component):
@@ -15,6 +17,14 @@ class MusicComponent(Utility_Component):
     current_music_state = {}
     _currently_playing = False
     _current_playlist = None
+    client = None
+
+    _cnt_dwn_timer = None
+
+    HOST = "localhost"
+    PORT = 6600
+
+    _connected = False
 
     def __init__(self, callback: Optional[Callable] = None) -> None:
         super().__init__(callback=callback)
@@ -30,8 +40,17 @@ class MusicComponent(Utility_Component):
         self._playlist_keys = None
         self._current_playlist = None
         self._currently_playing = False
+        self._cnt_dwn_timer = None
+
+        self._connected = False
 
         self.target_state(self.current_music_state)
+
+        self.client = MPDClient()
+        self.client.timeout = 10
+        self.client.idletimeout = None
+
+        self.update_playlists()
 
     def target_state(self, desired_state):
         # resetting values
@@ -67,24 +86,44 @@ class MusicComponent(Utility_Component):
 
         self.actual_state(self.current_music_state)
 
-    def playlist_change(self, state):
-        # TODO: mpc load playlist
-        pass
-        # print(state)
+    def update_playlists(self):
+        self._connect()
+        self.client.update()
 
-    def _load_playlist(self):
-        raise NotImplementedError
+    def playlist_change(self, state):
+        self._connect()
+        idx, _ = state
+        self.client.load(idx)
 
     def _play(self):
         self._currently_playing = True
-        raise NotImplementedError
+        self._connect()
+        self.client.play()
 
     def _stop(self):
         self._currently_playing = False
-        raise NotImplementedError
+        self._connect()
+        self.client.stop()
 
     def _next(self):
-        raise NotImplementedError
+        self._connect()
+        self.client.next()
 
     def _get_current_song(self):
-        raise NotImplementedError
+        self._connect()
+        return self.client.currentsong()
+
+    def quit(self):
+        self.client.close()
+        self.client.disconnect()
+
+    def _connect(self):
+        if not self._connected:
+            self.client.connect(self.HOST, self.PORT)
+            self._connected = True
+            self.cnt_dwn_timer = Timer(5, self.disconnect)
+            self.cnt_dwn_timer.start()
+
+    def disconnect(self):
+        self._connected = False
+        self.client.disconnect()
