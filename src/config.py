b58b74d774ee86067ac6bee91332e104f0024f5b
Den-Prahms-ihr-Ivo <ivo@ivolino.de>
2024-07-14 22:10:50 +0200

    Floetzinger

--- src/config.py
+++ src/config.py
@@ -1,4 +1,11 @@
+import os
 import configparser
 
 CONFIG = configparser.ConfigParser()
 CONFIG.read("./tools.ini")
+
+
+if os.environ.get("TESTING_ENV", None) is None:
+    CONFIG.read("/home/ivo/mood_light/mood_lighting/tools.ini")
+else:
+    CONFIG.read("./tools.ini")
