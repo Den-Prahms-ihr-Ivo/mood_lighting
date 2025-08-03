6dceeba743e016b23b12bb377d27a2c2a0c81cb1
Den-Prahms-ihr-Ivo <ivo@ivolino.de>
2025-08-03 19:32:16 +0200

    weitr geths

--- src/new_mood_light.py
+++ src/new_mood_light.py
@@ -53,7 +53,7 @@ def wheel(pos):
 def animate(ls):
     s = datetime.datetime.now().second
     m = datetime.datetime.now().minute
-    offset = int(s / 59 * LED_COUNT)
+    offset = int(s / 59 * (LED_COUNT - 1))
     minute_offset = m / 59 * 100
 
     R = int(CONFIG["DEFAULT"].get("COLOR_R", 0))
@@ -62,10 +62,10 @@ def animate(ls):
 
     for i in range(0, LED_COUNT):
         position = (i + offset) % LED_COUNT
-        color_intensity = i / LED_COUNT
-        r = int((R + minute_offset * 1) * color_intensity % 255)
-        g = int((G + minute_offset * 2) * color_intensity % 255)
-        b = int((B + minute_offset * 3) * color_intensity % 255)
+        color_intensity = i / (LED_COUNT - 1)
+        r = int(((R + minute_offset * 1) * color_intensity) % 255)
+        g = int(((G + minute_offset * 2) * color_intensity) % 255)
+        b = int(((B + minute_offset * 3) * color_intensity) % 255)
         ls.setPixelColor(position, Color(r, g, b))
 
     ls.show()
