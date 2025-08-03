749be50afb3b8bd941e86af07fa70b7e135c4b76
Den-Prahms-ihr-Ivo <ivo@ivolino.de>
2025-08-03 20:24:35 +0200

    weiter test

--- src/new_mood_light.py
+++ src/new_mood_light.py
@@ -69,15 +69,9 @@ def animate(ls):
     for i in range(0, LED_COUNT):
         position = (i + offset) % LED_COUNT
         color_intensity = i / (LED_COUNT - 1)
-        r = int(
-            (((R + minute_offset * 1 + delta.seconds) % 255) * color_intensity) % 255
-        )
-        g = int(
-            (((G + minute_offset * 2 + delta.seconds) % 255) * color_intensity) % 255
-        )
-        b = int(
-            (((B + minute_offset * 3 + delta.seconds) % 255) * color_intensity) % 255
-        )
+        r = int((((R + minute_offset * 1 + delta.seconds) * color_intensity % 255)))
+        g = int((((G + minute_offset * 2 + delta.seconds) * color_intensity % 255)))
+        b = int((((B + minute_offset * 3 + delta.seconds) * color_intensity % 255)))
         ls.setPixelColor(position, Color(r, g, b))
 
     ls.show()
